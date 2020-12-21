c173a2e773de6e1d5a11636288fb54d4c792d0e5
Nathan Getachew <nathygech111@gmail.com>
2020-12-21 16:37:33 +0300

    Update index.js
    - Add routes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// save images to images directory
+app.use('/images', express.static(path.join(__dirname, '../images')));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // global variables
-const PORT = process.env.PORT || 3000;
-const DB_URL = '';
+const PORT = process.env.PORT || 3003;
+const DB_URL = 'mongodb://localhost:27017/ecommerce';
+mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true });
 
 app.use(cors({
     origin: ['*'],
     exposedHeaders: ['auth-token']
 }));
 
+// routes
+const userRoutes = require('./router/userRoute');
+const itemRoutes = require('./router/itemRoute');
+const cartRoutes = require('./router/cartRoute');
+app.use('/api/users', userRoutes);
+app.use('/api/items', itemRoutes);
+app.use('/api/carts', cartRoutes);
 
 const server = app.listen(PORT, (_) => {
     console.log(`app listening on PORT ${PORT}`);
