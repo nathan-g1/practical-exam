4caa80442fab62ae50cd13ce95ec7684e805f844
Nathan Getachew <nathygech111@gmail.com>
2020-12-22 21:36:25 +0300

    :construction: WIP on api documentation

--- server/index.js
+++ server/index.js
@@ -5,13 +5,18 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // save images to images directory
 app.use('/images', express.static(path.join(__dirname, '../images')));
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // global variables
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 5000;
 const DB_URL = 'mongodb://localhost:27017/ecommerce';
 mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true });
 
